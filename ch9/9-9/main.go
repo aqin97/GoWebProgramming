1fbb83a09aa9e7c96e35b18eb6af092d78d59131
aqin97 <791041423@qq.com>
2021-06-17 14:33:19 +0800

    mosaic

--- ch9/9-9/main.go
+++ ch9/9-9/main.go
@@ -1,30 +1,96 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
+	"html/template"
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
-func callerA(c chan<- string) {
-	c <- "hello world"
-}
-
-func callerB(c chan<- string) {
-	c <- "nihao"
-}
-
 func main() {
-	a := make(chan string)
-	b := make(chan string)
-	go callerA(a)
-	go callerB(b)
-	for i := 0; i < 5; i++ {
-		select {
-		case msg := <-a:
-			fmt.Printf("%s from A\n", msg)
-		case msg := <-b:
-			fmt.Printf("%s from B\n", msg)
-		default:
-			fmt.Println("default")
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.HandleFunc("/", upload)
+	mux.HandleFunc("/mosaic", mosaic)
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+
+	TILESDB = tileDB()
+	fmt.Println("mosaic server started")
+
+	server.ListenAndServe()
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("upload.html")
+	t.Execute(w, nil)
+}
+
+func mosaic(w http.ResponseWriter, r *http.Request) {
+	t0 := time.Now()
+
+	r.ParseMultipartForm(10485760)
+	file, _, _ := r.FormFile("image")
+	defer file.Close()
+
+	tileSize, _ := strconv.Atoi(r.FormValue("tile_size"))
+
+	original, _, _ := image.Decode(file)
+	bounds := original.Bounds()
+
+	newimage := image.NewNRGBA(image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y))
+
+	db := cloneTilesDB()
+
+	sp := image.Point{0, 0}
+	for y := bounds.Min.Y; y < bounds.Max.Y; y += tileSize {
+		for x := bounds.Min.X; x < bounds.Max.X; x += tileSize {
+			r, g, b, _ := original.At(x, y).RGBA()
+			color := [3]float64{float64(r), float64(g), float64(b)}
+
+			nearest := nearest(color, &db)
+			file, err := os.Open(nearest)
+			if err != nil {
+				fmt.Println("error:", nearest)
+			} else {
+				img, _, err := image.Decode(file)
+				if err != nil {
+					fmt.Println("error:", err, nearest)
+				} else {
+					t := resize(img, tileSize)
+					tile := t.SubImage(t.Bounds())
+					tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
+					draw.Draw(newimage, tileBounds, tile, sp, draw.Src)
+				}
+			}
+			file.Close()
 		}
 	}
+
+	buf1 := new(bytes.Buffer)
+	jpeg.Encode(buf1, original, nil)
+	originalStr := base64.StdEncoding.EncodeToString(buf1.Bytes())
+
+	buf2 := new(bytes.Buffer)
+	jpeg.Encode(buf2, newimage, nil)
+	newimageStr := base64.StdEncoding.EncodeToString(buf2.Bytes())
+
+	t1 := time.Now()
+	images := map[string]string{
+		"original": originalStr,
+		"mosaic":   newimageStr,
+		"duration": fmt.Sprintf("%v", t1.Sub(t0)),
+	}
+	t, _ := template.ParseFiles("results.html")
+	t.Execute(w, images)
 }
--- ch9/9-9/mosaic.go
+++ ch9/9-9/mosaic.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"image"
+	"image/color"
+	"io/ioutil"
+	"math"
+	"os"
 )
 
 //求取平均颜色的函数
@@ -21,7 +26,70 @@ func averageColor(img image.Image) [3]float64 {
 //缩放到指定大小
 func resize(in image.Image, newWidth int) image.NRGBA {
 	bound := in.Bounds()
-	ratio := bound.Dx()/newWidth
+	ratio := bound.Dx() / newWidth
 	out := image.NewNRGBA(image.Rect(bound.Min.X/ratio, bound.Min.Y/ratio, bound.Max.X/ratio, bound.Max.Y/ratio))
-	for y, j := bound.Min.Y
+	for y, j := bound.Min.Y, bound.Min.Y; y < bound.Max.Y; y, j = y+ratio, j+1 {
+		for x, i := bound.Min.X, bound.Min.X; x < bound.Max.X; x, i = x+ratio, i+1 {
+			r, g, b, a := in.At(x, y).RGBA()
+			out.SetNRGBA(i, j, color.NRGBA{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)})
+		}
+	}
+	return *out
+}
+
+//从tiles文件夹中扫描图片，创建瓷砖图片数据库
+func tileDB() map[string][3]float64 {
+	fmt.Println("start populating tiles db ...")
+	db := make(map[string][3]float64)
+	files, _ := ioutil.ReadDir("tiles")
+	for _, f := range files {
+		name := "tiles/" + f.Name()
+		file, err := os.Open(name)
+		if err != nil {
+			fmt.Println("connot open file", name, err)
+		} else {
+			img, _, err := image.Decode(file)
+			if err != nil {
+				fmt.Println("error in populating tiledb", err, name)
+			} else {
+				db[name] = averageColor(img)
+			}
+		}
+		file.Close()
+	}
+	fmt.Println("finished populating tiles db")
+	return db
+}
+
+//匹配和瓷砖最近的平均颜色
+func nearest(target [3]float64, db *map[string][3]float64) string {
+	var filename string
+	smallest := 1000000.0
+	for k, v := range *db {
+		dist := distance(target, v)
+		if dist < smallest {
+			filename, smallest = k, dist
+		}
+	}
+	delete(*db, filename)
+	return filename
+}
+
+//求两点之间的欧几里得距离
+func distance(p1, p2 [3]float64) float64 {
+	return math.Sqrt(sq(p1[0]-p2[0]) + sq(p1[1]-p2[1]) + sq(p1[2]-p2[2]))
+}
+
+func sq(n float64) float64 {
+	return n * n
+}
+
+var TILESDB map[string][3]float64
+
+func cloneTilesDB() map[string][3]float64 {
+	db := make(map[string][3]float64)
+	for k, v := range TILESDB {
+		db[k] = v
+	}
+	return db
 }
